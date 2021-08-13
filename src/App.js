73f299911e278e8503314579c814fb5be97a61d0
stevenjhomem <sjhomem@outlook.com>
2021-08-13 08:46:18 -0500

    Finished getting my array from the api using the effect hook only once and setting it into state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
+
+  const [characterList, setCharacterList]= useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(response => {
+      console.log(response.data);
+      setCharacterList(response.data)
+    })
+    .catch(error => console.error('No Information Received'))
+    },[]);
 
   return (
     <div className="App">
